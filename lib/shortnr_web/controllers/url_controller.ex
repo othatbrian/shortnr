537ebfe9e9f1e62dc096600e818c3e8cab82fe8b
Brian Stevens <brian.stevens@dataporters.com>
2021-09-11 09:40:11 -0500

    Remove unnecessary UrlController actions

--- lib/shortnr_web/controllers/url_controller.ex
+++ lib/shortnr_web/controllers/url_controller.ex
@@ -4,11 +4,6 @@ defmodule ShortnrWeb.UrlController do
   alias Shortnr.Urls
   alias Shortnr.Urls.Url
 
-  def index(conn, _params) do
-    urls = Urls.list_urls()
-    render(conn, "index.html", urls: urls)
-  end
-
   def new(conn, _params) do
     changeset = Urls.change_url(%Url{})
     render(conn, "new.html", changeset: changeset)
@@ -29,34 +24,5 @@ defmodule ShortnrWeb.UrlController do
   def show(conn, %{"id" => id}) do
     url = Urls.get_url!(id)
     render(conn, "show.html", url: url)
-  end
-
-  def edit(conn, %{"id" => id}) do
-    url = Urls.get_url!(id)
-    changeset = Urls.change_url(url)
-    render(conn, "edit.html", url: url, changeset: changeset)
-  end
-
-  def update(conn, %{"id" => id, "url" => url_params}) do
-    url = Urls.get_url!(id)
-
-    case Urls.update_url(url, url_params) do
-      {:ok, url} ->
-        conn
-        |> put_flash(:info, "Url updated successfully.")
-        |> redirect(to: Routes.url_path(conn, :show, url))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", url: url, changeset: changeset)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    url = Urls.get_url!(id)
-    {:ok, _url} = Urls.delete_url(url)
-
-    conn
-    |> put_flash(:info, "Url deleted successfully.")
-    |> redirect(to: Routes.url_path(conn, :index))
   end
 end
--- test/shortnr_web/controllers/url_controller_test.exs
+++ test/shortnr_web/controllers/url_controller_test.exs
@@ -1,18 +1,8 @@
 defmodule ShortnrWeb.UrlControllerTest do
   use ShortnrWeb.ConnCase
 
-  import Shortnr.UrlsFixtures
-
   @create_attrs %{long_url: "some long_url", short_url: "some short_url"}
-  @update_attrs %{long_url: "some updated long_url", short_url: "some updated short_url"}
   @invalid_attrs %{long_url: nil, short_url: nil}
-
-  describe "index" do
-    test "lists all urls", %{conn: conn} do
-      conn = get(conn, Routes.url_path(conn, :index))
-      assert html_response(conn, 200) =~ "Listing Urls"
-    end
-  end
 
   describe "new url" do
     test "renders form", %{conn: conn} do
@@ -36,49 +26,5 @@ defmodule ShortnrWeb.UrlControllerTest do
       conn = post(conn, Routes.url_path(conn, :create), url: @invalid_attrs)
       assert html_response(conn, 200) =~ "New Url"
     end
-  end
-
-  describe "edit url" do
-    setup [:create_url]
-
-    test "renders form for editing chosen url", %{conn: conn, url: url} do
-      conn = get(conn, Routes.url_path(conn, :edit, url))
-      assert html_response(conn, 200) =~ "Edit Url"
-    end
-  end
-
-  describe "update url" do
-    setup [:create_url]
-
-    test "redirects when data is valid", %{conn: conn, url: url} do
-      conn = put(conn, Routes.url_path(conn, :update, url), url: @update_attrs)
-      assert redirected_to(conn) == Routes.url_path(conn, :show, url)
-
-      conn = get(conn, Routes.url_path(conn, :show, url))
-      assert html_response(conn, 200) =~ "some updated long_url"
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, url: url} do
-      conn = put(conn, Routes.url_path(conn, :update, url), url: @invalid_attrs)
-      assert html_response(conn, 200) =~ "Edit Url"
-    end
-  end
-
-  describe "delete url" do
-    setup [:create_url]
-
-    test "deletes chosen url", %{conn: conn, url: url} do
-      conn = delete(conn, Routes.url_path(conn, :delete, url))
-      assert redirected_to(conn) == Routes.url_path(conn, :index)
-
-      assert_error_sent 404, fn ->
-        get(conn, Routes.url_path(conn, :show, url))
-      end
-    end
-  end
-
-  defp create_url(_) do
-    url = url_fixture()
-    %{url: url}
   end
 end
