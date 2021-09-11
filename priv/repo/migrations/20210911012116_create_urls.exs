6b1135b240d1a5abd60fba3f503771f8af4eab6f
Brian Stevens <brian.stevens@dataporters.com>
2021-09-10 20:43:27 -0500

    Remove primary keys from urls

--- priv/repo/migrations/20210911012116_create_urls.exs
+++ priv/repo/migrations/20210911012116_create_urls.exs
@@ -3,7 +3,6 @@ defmodule Shortnr.Repo.Migrations.CreateUrls do
 
   def change do
     create table(:urls, primary_key: false) do
-      add :id, :binary_id, primary_key: true
       add :short_url, :string
       add :long_url, :string
 
