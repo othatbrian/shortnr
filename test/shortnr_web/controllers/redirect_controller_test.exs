c6df9e1f7c389cdedf1de2890ea355c288cc1f9a
Brian Stevens <brian.stevens@dataporters.com>
2021-09-12 16:43:16 -0500

    Fix warning in test

--- test/shortnr_web/controllers/redirect_controller_test.exs
+++ test/shortnr_web/controllers/redirect_controller_test.exs
@@ -2,7 +2,6 @@ defmodule ShortnrWeb.RedirectControllerTest do
   use ShortnrWeb.ConnCase
 
   @create_attrs %{long_url: "http://www.example.com", short_url: "abcd"}
-  @invalid_attrs %{long_url: nil, short_url: nil}
 
   describe "redirect" do
     test "succeeds when short URL is valid", %{conn: conn} do
