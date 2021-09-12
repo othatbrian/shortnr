7270cb6d5c43d85b3b693a59d68941b62b1a36d6
Brian Stevens <brian.stevens@dataporters.com>
2021-09-12 17:50:22 -0500

    Provide better error message

--- lib/shortnr/urls/url.ex
+++ lib/shortnr/urls/url.ex
@@ -17,6 +17,6 @@ defmodule Shortnr.Urls.Url do
     url
     |> cast(attrs, [:short_url, :long_url])
     |> validate_required([:short_url, :long_url])
-    |> validate_format(:long_url, ~r/^https?:\/\//)
+    |> validate_format(:long_url, ~r/^https?:\/\//, message: "URL must contain 'http://' or 'https://'")
   end
 end
